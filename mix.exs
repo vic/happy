a753d94233df16eb9bf49ee3b50a1c34fc2ba6ca
Victor Borja <vborja@apache.org>
2016-01-18 09:43:03 -0600

    relax elixir dependency, be happy

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Happy.Mixfile do
   def project do
     [app: :happy,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.0",
      description: description,
      package: package,
      build_embedded: Mix.env == :prod,
